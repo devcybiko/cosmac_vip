5f9a8df349b17f4400fd80f6a8bcf90b16d7d0c9
Gregory Smith <greg@agilefrontiers.com>
2025-09-20 22:55:10 -0400

    adding starship aanimation

--- assets.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <avr/pgmspace.h>
-
-extern const uint8_t PROGMEM PLAYER_BMP[];
--- game.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-void gameInit();
-void gameUpdate();
-void gameDraw();
